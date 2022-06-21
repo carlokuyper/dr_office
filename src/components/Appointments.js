564c77a44f9cdd68fafc3a714f0851715029f26e
carlokuyper <180145@virtualwindow.co.za>
2022-06-21 11:08:57 +0200

    started with functionality

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -6,13 +6,24 @@ import '../css/appointments.css';
 import {useState, useRef, useEffect } from 'react';
 import axios from "axios";
 
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 const Appointments = () => {
+
+    const [value, onChange] = useState(new Date());
+
     return(
         <>
         <Header />
         <div className="container">
             <div className="calendar">
-                
+              <div>
+                <Calendar
+                  onChange={onChange}
+                  value={value}
+                />
+              </div>
             </div>
         </div>
         </>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import '../css/login.css'
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: ''
+  });
+
+  const usernameVal = (e) => {
+    const value = e.target.value;
+    setInputs({...inputs, username: value});
+    //Here you will validate 
+  }
+
+  const passwordeVal = (e) => {
+    const value = e.target.value;
+    setInputs({...inputs, password: value});
+    //Here you will validate 
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+
+    axios.post('http://localhost:80/drOffice/userLogin.php', inputs)
+        .then(function(response){
+          console.log(response);
+
+          if(response.data === true){
+            sessionStorage.setItem('activeUser', inputs.username);
+            navigate("/dashboard")
+          }else {
+            console.log("not working");
+          }
+
+        });
+
+  }
+
     return(
         <div className="login-container">
             <div className="logo-holder-long"><img className="logo-img-long" src="../logo-long-white.png"/></div>
@@ -10,12 +52,20 @@ const Login = () => {
             <div className="login-con">
                 <h2>Login</h2>
                 <form>
-                    <input className="login-input" name="username" placeholder="username"/>
-                    <input className="login-input" name="password" placeholder="password"/>
+                    <input className="login-input" name="username" type="username" placeholder="username" onChange={usernameVal} />
+                    <input className="login-input" name="password" type="password" placeholder="password" onChange={passwordeVal} />
                 </form>
                 <a><p className="forgot-btn">Forgot password</p></a>
-                <button className="login-btn" type="submit">Login</button>
+                <button className="login-btn" type="submit"  onClick={handleSubmit} >Login</button>
             </div>
+
+        {/* <form>
+            <h1>Sign In to FakeBook</h1>
+            <p>You life belongs to us!</p>
+            <input name="username" type="username" placeholder='Your Username' onChange={usernameVal} />
+            <input name="password" type="password" placeholder='YourPassword' onChange={passwordeVal} />     
+            <button type="submit" onClick={handleSubmit}>I am ready to waste my life!</button>
+        </form> */}
 
         </div>
     );
